0b596b6484db825e1c959329f568b84ce08a25a2
Andreas Säuberli <aandivb@gmail.com>
2015-12-12 10:59:25 +0100

    gaggi

--- web/input.dart
+++ web/input.dart
@@ -1,2 +1,36 @@
 part of ld34;
 
+class Input {
+
+  bool leftMouse, rightMouse;
+  int mouseX, mouseY;
+
+  void init() {
+    leftMouse = false;
+    rightMouse = false;
+    mouseX = 0;
+    mouseY = 0;
+  }
+
+  void onMouseDown(MouseEvent event) {
+    if (event.button == 0) {
+      leftMouse = true;
+    } else if (event.button = 2) {
+      rightMouse = true;
+    }
+  }
+
+  void onMouseUp(MouseEvent event) {
+    if (event.button == 0) {
+      leftMouse = false;
+    } else if (event.button = 2) {
+      rightMouse = false;
+    }
+  }
+
+  void onMouseMove(MouseEvent event) {
+    mouseX = event.layer.x;
+    mouseY = event.layer.y;
+  }
+
+}
--- web/resources.dart
+++ web/resources.dart
@@ -1,2 +1,17 @@
 part of ld34;
 
+class Resources {
+
+  static Map<String, ImageElement> images;
+  static Map<String, AudioElement> sounds;
+
+  static void init() {
+    images = new Map<String, ImageElement>();
+    sounds = new Map<String, AudioElement>();
+  }
+
+  static void load() {
+    sounds['test'] = new AudioElement('res/test.wav');
+  }
+
+}
