cfb6ee49f48d3e080bad272e9649a5ff0bed8115
Andreas Säuberli <aandivb@gmail.com>
2015-12-12 09:46:09 +0100

    files

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,14 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+library ld34;
 
 import 'dart:html';
 import 'dart:math';
+
+part 'gamestate.dart';
+part 'world.dart';
+part 'bacterium.dart';
+part 'nutrient.dart';
+part 'input.dart';
+part 'resources.dart';
 
 CanvasElement canvas;
 CanvasRenderingContext2D canvasContext;
@@ -14,13 +20,17 @@ void main() {
   canvasContext = canvas.context2D;
   random = new Random();
   bacteria = new List<Bacterium>();
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 10000; i++) {
     bacteria.add(new Bacterium(random.nextInt(400) + 200, random.nextInt(200) + 125));
   }
   requestFrame();
 }
 
 void frame(num time) {
+  canvasContext.fillStyle = '#FFF';
+  canvasContext.globalAlpha = 1;
+  canvasContext.fillRect(0, 0, 800, 450);
+  canvasContext.globalAlpha = 1;
   for (int i = 0; i < bacteria.length; i++) {
     bacteria[i].update();
     bacteria[i].draw();
@@ -41,13 +51,13 @@ class Bacterium {
   }
 
   void update() {
-    x += random.nextInt(3) - 1;
-    y += random.nextInt(3) - 1;
+    x += (random.nextInt(3) - 1) / 10;
+    y += (random.nextInt(3) - 1) / 10;
   }
 
   void draw() {
     canvasContext.fillStyle = '#F00';
-    canvasContext.fillRect(x, y, 2, 2);
+    canvasContext.fillRect(x, y, 4, 4);
   }
 
 }
